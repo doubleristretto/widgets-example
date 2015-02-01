1342ccaba7847b66a53037bac54052d64c8b5b3c
doubleristretto <doubleristretto@users.noreply.github.com>
2015-02-02 01:28:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/uiwidgets/uiwidgets/MainListActivity.java
@@ -0,0 +1,52 @@
+package com.example.uiwidgets.uiwidgets;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by jeanniel
+ */
+public class MainListActivity extends ListActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setTitle(R.string.app_name);
+
+        // Create a new Adapter containing a list of colors
+        // Set the adapter on this ListActivity's built-in ListView
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.widget_items)));
+
+        ListView lv = getListView();
+
+        // Enable filtering when the user types in the virtual keyboard
+        lv.setTextFilterEnabled(true);
+
+        // Set an setOnItemClickListener on the ListView
+        lv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Intent intent = new Intent();
+
+                switch (position) {
+                    case 0:
+                        intent.setClass(view.getContext(), ButtonActivity.class);
+                        startActivity(intent);
+                    case 1:
+                        intent.setClass(view.getContext(), RadioGroupActivity.class);
+                        startActivity(intent);
+                }
+
+
+            }
+        });
+    }
+}
