3065834ea21ca8380759cae0dd7efc242a8d795b
doubleristretto <doubleristretto@users.noreply.github.com>
2015-02-02 10:33:37 +0800

    Added example code for date picker

---
+++ app/src/main/java/com/example/uiwidgets/uiwidgets/DatePickerActivity.java
@@ -0,0 +1,18 @@
+package com.example.uiwidgets.uiwidgets;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * This is the date picker activity
+ */
+public class DatePickerActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datepicker);
+
+    }
+}
